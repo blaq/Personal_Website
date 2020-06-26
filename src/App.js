cc09194d586ccef0360ab393910e10f95a38c6a1
blaq <43507910+blaq@users.noreply.github.com>
2020-06-26 14:31:44 -0700

    content template is complete

--- src/App.js
+++ src/App.js
@@ -30,18 +30,29 @@ function App() {
 
           <Container>
             <Row>
-              <h2>About</h2>
+              <Col>
+                <h2>About</h2>
+              </Col>
             </Row>
             <Row>
               <Col sm={8}>
                 <p>
-                  Hello this is a para
+                  Professionally, I am a recent graduate that is preparing for an exciting career in technology. My near future goal is to work as a software engineer to learn how to work with others in a professional environment. My far reaching goals are to work with expanding tech such as blockchain, cloud computing, or something that hasn't arrived yet.
+                </p>
+                <p>
+                  Casually, I like to run 5ks and play video games. My fastest 5k time is 16:23 and my favorite video game is Halo Reach. The artists I listen to the most are Radiohead, Portishead, and Bjork. When I have the time, I also programming on my own time. I will learn new languages and software to pride myself with creating something interesting.
                 </p>
               </Col>
               <Col sm={2}>
+                <p>Aspirations</p>
                 <ul>
-                  <li>Organized</li>
+                  <li>Adaptive</li>
+                  <li>Decisive</li>
+                  <li>Experimental</li>
+                  <li>Humble</li>
                   <li>Motivated</li>
+                  <li>Respectful</li>
+                  <li>Passionate</li>
                 </ul>
               </Col>
             </Row>
@@ -49,7 +60,9 @@ function App() {
           
           <Container>
             <Row>
-              <h2>Skills</h2>
+              <Col>
+                <h2>Skills</h2>
+              </Col>
             </Row>
             <Row>
               <Col>
@@ -92,7 +105,9 @@ function App() {
 
           <Container>
             <Row>
-              <h2>Projects</h2>
+              <Col>
+                <h2>Projects</h2>
+              </Col>
             </Row>
             <Row>
               <Col>
@@ -143,7 +158,118 @@ function App() {
             </Row>
           </Container>
 
-          
+          <Container>
+            <Row>
+              <Col>
+                <h2>Education</h2>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={8}>
+                <h3>Western Oregon Univesity</h3>
+                <h5>Bachelor of Science in Computer Science</h5>
+                <h5>Minor in Mathematics</h5>
+              </Col>
+              <Col sm={2}>
+                <p>-WOU PIC-</p>
+                <p>Graduated</p>
+                <p>June 2020</p>
+              </Col>
+              <Row>
+                <Col>
+                  <h5 align="center">Notable Coursework</h5>
+                  <Row>
+                    <Col sm={6}>
+                      <ul>
+                        <li>Blockchain Tech and Cryptocurrency</li>
+                        <li>Concurrent Systems</li>
+                        <li>Data Mining and Data Warehouse</li>
+                      </ul>
+                    </Col>
+                    <Col sm={6}>
+                      <ul>
+                        <li>Information Management</li>
+                        <li>Microservice Architecture</li>
+                        <li>Number Theory</li>
+                      </ul>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Row>
+          </Container>
+
+          <Container>
+            <Row>
+              <Col>
+                <h2>Experience</h2>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <h3>IT Technician Intern</h3>
+                <h5>Miller Technologies - McMinnville, Oregon</h5>
+                <Row>
+                  <Col sm={8}>
+                    <ul>
+                      <li>Monitored and maintained IT infrastructure for multiple businesses</li>
+                      <li>Collaborated to develop and improve upon proprietary software</li>
+                    </ul>  
+                  </Col>
+                  <Col sm={2}>
+                    <p>Period</p>
+                    <ul>
+                      <li>May 2019</li>
+                      <li>March 2020</li>
+                    </ul>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <h3>Digital Media Center Tutor</h3>
+                <h5>Western Oregon University - Monmouth, Oregon</h5>
+                <Row>
+                  <Col sm={8}>
+                    <ul>
+                      <li>Tutored students and professors with Adobe and other digital media tools</li>
+                      <li>Gave expert solutions on a diverse range of digital media formats</li>
+                    </ul>  
+                  </Col>
+                  <Col sm={2}>
+                    <p>Period</p>
+                    <ul>
+                      <li>April 2019</li>
+                      <li>June 2020</li>
+                    </ul>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <h3>Domino’s Delivery Driver</h3>
+                <h5>Talen Pizza - Albany, Oregon</h5>
+                <Row>
+                  <Col sm={8}>
+                    <ul>
+                      <li>Assisted in the opening of a new store and training new employees</li>
+                      <li>Routed deliveries and exceeded customer expectations</li>
+                    </ul>  
+                  </Col>
+                  <Col sm={2}>
+                    <p>Period</p>
+                    <ul>
+                      <li>Sept 2016</li>
+                      <li>Jan 2020</li>
+                    </ul>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+
         </Col>
       </Row>
     </Container>
